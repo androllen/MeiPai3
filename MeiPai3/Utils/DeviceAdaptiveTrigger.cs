6d4839f74e3f82658194586cf7540385c9071bb5
androllen <androllen@hotmail.com>
2016-04-28 12:06:52 +0800

    add adaptive trigger

---
+++ MeiPai3/Utils/DeviceAdaptiveTrigger.cs
@@ -0,0 +1,91 @@
+﻿/********************************************************************************
+** 作者： androllen
+** 日期： 16/4/27 18:52:15
+** 微博： http://weibo.com/Androllen
+*********************************************************************************/
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeYa.Core;
+using WeYa.Domain;
+using Windows.UI.Xaml;
+using MeiPai3.ViewModels;
+using Caliburn.Micro;
+
+namespace MeiPai3.Trigger
+{
+    public enum AdaptiveType
+    {
+        Show1,
+        Show2,
+        Show12
+    }
+    public class DeviceAdaptiveTrigger : StateTriggerBase
+    {
+        private readonly INotifyFrameChanged _frameManager;
+        //private readonly GlobalInfoManager _globalInfoManager;
+        private const double minwidth = 648;
+        private const double maxwidth = 800;
+        public DeviceAdaptiveTrigger()
+        {
+            Window.Current.SizeChanged += Current_SizeChanged;
+            _frameManager = IoC.Get<INotifyFrameChanged>();
+        }
+
+        private void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e)
+        {
+            var _adaptiveType = AdaptiveDevice(e.Size.Width);
+            SetActive(_adaptiveType == adaptiveType);
+        }
+        public AdaptiveType AdaptiveDevice(double width)
+        {
+            if(_frameManager.ContentFrame?.Content !=null 
+                && _frameManager.ContentFrame.Content.GetType() == typeof(CollectViewModel))
+            {
+                return AdaptiveType.Show2;
+            }
+            else
+            {
+                if (width > 0 && width <= minwidth)
+                {
+                    if (_frameManager.IsHasContent())
+                    {
+                        return AdaptiveType.Show2;
+                    }
+                    else
+                        return AdaptiveType.Show1;
+                }
+                else
+                    return AdaptiveType.Show12;
+            }
+
+        }
+
+        private AdaptiveType _adaptiveType;
+        public AdaptiveType adaptiveType
+        {
+            get { return _adaptiveType; }
+            set
+            {
+                _adaptiveType = value;
+                var device = AdaptiveDevice(Window.Current.Bounds.Width);
+                SetActive(device == _adaptiveType);
+            }
+        }
+
+        //public static readonly DependencyProperty MinWindowWidthProperty = 
+        //    DependencyProperty.Register(
+        //    "MinWindowWidth", 
+        //    typeof(double), 
+        //    typeof(DeviceAdaptiveTrigger), 
+        //    new PropertyMetadata(0.0));
+
+        //public double MinWindowWidth
+        //{
+        //    get { return (double)GetValue(MinWindowWidthProperty); }
+        //    set { SetValue(MinWindowWidthProperty, value); }
+        //}
+    }
+}
---
+++ MeiPai3/ViewModels/InitMainViewModel.cs
@@ -0,0 +1,100 @@
+﻿/********************************************************************************
+** 作者： androllen
+** 日期： 16/4/12 19:15:40
+** 微博： http://weibo.com/Androllen
+*********************************************************************************/
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using WeYa.Tools.Utils;
+using System.Collections.ObjectModel;
+using Windows.ApplicationModel;
+using WeYa.Core;
+
+namespace MeiPai3.ViewModels
+{
+    public class InitMainViewModel : BaseViewModel
+    {
+        private int currentPage = 1;
+        public IncrementalLoadingCollection<IncrementedItem> InfiniteItems { get; set; }
+
+        private string _title;
+        public override string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    NotifyOfPropertyChange(()=> Title);
+                }
+            }
+        }  
+
+        public InitMainViewModel(INotifyFrameChanged frame) 
+            : base(frame)
+        {
+            InitializeData();
+
+        }
+        protected override void OnActivate()
+        {
+            base.OnActivate();
+        }
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+
+        }
+        protected override void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+
+        }
+        private void InitializeData()
+        {
+            if (DesignMode.DesignModeEnabled)
+                return;
+
+            //when instantiating the collection, you pass it the task that gets more item, in this case it's "GetMoreData"
+            //NOTE: This is for infinite items. if you have a max or total count, use the overload and pass the total as the 2nd parameter
+            InfiniteItems = new IncrementalLoadingCollection<IncrementedItem>((cancellationToken, count) => Task.Run(GetMoreData, cancellationToken));
+            //InfiniteItems = new IncrementalLoadingCollection<IncrementedItem>((cancellationToken, count) => Task.Run(GetMoreData, cancellationToken), 150,(isha)=>Task.Run(getMoreOver));
+            //InfiniteItems = new IncrementalLoadingCollection<IncrementedItem>((cancellationToken, count) => Task.Run(GetMoreData, cancellationToken), 450, () =>
+            //{
+            //    System.Diagnostics.Debug.WriteLine("sss");
+            //});
+        }
+
+        private async Task<ObservableCollection<IncrementedItem>> GetMoreData()
+        {
+            //I'm just simulating an API that supports paging
+            return await FakeApiCallAsync(currentPage++, 25);
+        }
+
+        //Super Awesome API :D
+        private async Task<ObservableCollection<IncrementedItem>> FakeApiCallAsync(int pageNumber, int itemsPerPage)
+        {
+            return await Task.Run(() =>
+            {
+                var items = new ObservableCollection<IncrementedItem>();
+
+                var startingRecordToUse = itemsPerPage * (pageNumber - 1);
+                var endingRecordToUse = startingRecordToUse + itemsPerPage;
+
+                for (int i = startingRecordToUse; i < endingRecordToUse; i++)
+                {
+                    items.Add(new IncrementedItem { Id = i, Title = $"Item {i}" });
+                }
+
+                return items;
+            });
+        }
+
+        public class IncrementedItem
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+        }
+    }
+}
