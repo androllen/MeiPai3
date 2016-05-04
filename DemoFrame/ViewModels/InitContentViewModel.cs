50ba6a5767d6029113adeb34be43a866d8c01052
androllen <androllen@hotmail.com>
2016-05-04 18:04:43 +0800

    add pivot header for test

--- DemoFrame/ViewModels/InitContentViewModel.cs
+++ DemoFrame/ViewModels/InitContentViewModel.cs
@@ -3,7 +3,12 @@
 ** 日期： 16/4/12 19:15:40
 ** 微博： http://weibo.com/Androllen
 *********************************************************************************/
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using WeYa.Core;
+using WeYa.Domain;
+using WeYa.Domain.Models;
 
 namespace DemoFrame.ViewModels
 {
@@ -22,10 +27,25 @@ namespace DemoFrame.ViewModels
                 }
             }
         }
+ 
+        public ObservableCollection<SampleDataModel> NewItems { get; set; }
+        public ObservableCollection<SampleDataModel> FlaggedItems { get; set; }
+        public ObservableCollection<SampleDataModel> AllItems { get; set; }
 
         public InitContentViewModel(INotifyFrameChanged frame) 
             : base(frame)
         {
+          var newItems = SampleDataModel.GetSampleData().Where(x => x.IsNew).ToList();
+            var flaggedItems = SampleDataModel.GetSampleData().Where(x => x.IsFlagged).ToList();
+           var allItems = SampleDataModel.GetSampleData().ToList();
+            this.NewItems = new ObservableCollection<SampleDataModel>();
+            this.FlaggedItems = new ObservableCollection<SampleDataModel>();
+            this.AllItems = new ObservableCollection<SampleDataModel>();
+
+            newItems.ForEach((b) => NewItems.Add(b));
+            flaggedItems.ForEach((b) => FlaggedItems.Add(b));
+            allItems.ForEach((b) => AllItems.Add(b));
+
         }
         public void ShowClickItem()
         {
--- DemoFrame/Views/InitContentView.xaml.cs
+++ DemoFrame/Views/InitContentView.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using WeYa.Domain;
+using WeYa.Domain.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -11,6 +13,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上提供
@@ -30,6 +33,44 @@ namespace DemoFrame.Views
             //var text = loader.GetString("StatusBar_Success");
             //var toast = new CCUWPToolkit.Controls.WYToastDialog();
             //toast.ShowAsync(text);
+        }
+        private void ShowItem(SampleDataModel model)
+        {
+            var MyDialog = new ContentDialog();
+
+            if (model != null)
+            {
+                var MyImage = new Image();
+                MyImage.Source = new BitmapImage(new Uri(model.ImagePath));
+                MyImage.HorizontalAlignment = HorizontalAlignment.Stretch;
+                MyImage.VerticalAlignment = VerticalAlignment.Stretch;
+                MyImage.Stretch = Windows.UI.Xaml.Media.Stretch.UniformToFill;
+                MyDialog.Title = String.Format("You have selected {0}", model.ToString());
+                MyDialog.Content = MyImage;
+            }
+            else
+            {
+                MyDialog.Title = "No item found";
+
+            }
+
+            MyDialog.PrimaryButtonText = "OK";
+
+            Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => MyDialog.ShowAsync());
+
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SampleDataModel SelectedItem = null;
+
+            if (e.AddedItems.Count >= 1)
+            {
+                SelectedItem = e.AddedItems[0] as SampleDataModel;
+                (sender as ListView).SelectedItem = null;
+                ShowItem(SelectedItem);
+            }
+
         }
     }
 }
